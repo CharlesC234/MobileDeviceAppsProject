8a82538babd6fabca0fbaaf127b7cc8b2c87ba6b
Rodolphe S. Eugene <92120435+RodolpheEugene@users.noreply.github.com>
2023-10-16 14:04:33 -0400

    New stuff

---
+++ TeamProject/app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/cahillcharles/TeamProject/LoginActivity.java
@@ -0,0 +1,47 @@
+package edu.floridapoly.mobiledeviceapps.fall23.cahillcharles.TeamProject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    // Declare button variables
+    Button loginBtn, signUpBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // login button listener
+        loginBtn = (Button) findViewById(R.id.loginButton);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainScreen.class);
+                startActivity(intent);
+
+                Toast toast = Toast.makeText(getApplicationContext(), "Proceeding to Home Screen", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        //  sign up button listener
+        signUpBtn = (Button) findViewById(R.id.signUpButton);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+
+                Toast toast = Toast.makeText(getApplicationContext(), "Proceed to Sign up", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+    }
+
+}
---
+++ TeamProject/app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/cahillcharles/TeamProject/SignUpActivity.java
@@ -0,0 +1,16 @@
+package edu.floridapoly.mobiledeviceapps.fall23.cahillcharles.TeamProject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    // Declare variables
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
