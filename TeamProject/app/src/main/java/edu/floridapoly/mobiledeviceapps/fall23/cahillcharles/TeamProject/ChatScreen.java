18ad82a147a9fef0ec5995ce792bf78d2058f9da
“CharlesCahill123” <charles.cahill.intern@sofwerx.org>
2023-10-16 14:59:19 -0400

    Testing by adding chatscreen

---
+++ TeamProject/app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/cahillcharles/TeamProject/ChatScreen.java
@@ -0,0 +1,14 @@
+package edu.floridapoly.mobiledeviceapps.fall23.cahillcharles.TeamProject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_screen);
+    }
+}
