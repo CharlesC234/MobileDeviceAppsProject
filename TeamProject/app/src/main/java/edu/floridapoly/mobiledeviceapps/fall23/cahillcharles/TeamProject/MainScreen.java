e2f99e786dd337f809171cfd81d6f0a5043748e1
Zachary <zacharychair@gmail.com>
2023-10-16 23:03:37 -0400

    Added comments + Main Screen + Profile Screen

--- TeamProject/app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/cahillcharles/TeamProject/MainScreen.java
+++ TeamProject/app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/cahillcharles/TeamProject/MainScreen.java
@@ -2,13 +2,43 @@ package edu.floridapoly.mobiledeviceapps.fall23.cahillcharles.TeamProject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainScreen extends AppCompatActivity {
-
+    Button chatBtn, profileBtn, workoutBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        chatBtn = (Button) findViewById(R.id.tochatbtn);
+
+        workoutBtn = (Button) findViewById(R.id.toWorkouts);
+    }
+    public void goToChat(View myView)
+    {
+        Intent intent = new Intent(MainScreen.this, ChatScreen.class);
+        startActivity(intent);
+
+        Toast toast = Toast.makeText(getApplicationContext(), "Proceeding to Chat Screen", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+    public void goToProfile(View myView)
+    {
+        Intent intent = new Intent(MainScreen.this, ProfileScreen.class);
+        startActivity(intent);
+
+        Toast toast = Toast.makeText(getApplicationContext(), "Proceeding to Profile Screen", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void toSavedWorkouts(View view) {
+        Intent intent = new Intent(MainScreen.this, SavedWorkouts.class);
+        startActivity(intent);
+        Toast.makeText(this, "Going to workouts", Toast.LENGTH_SHORT).show();
     }
 }
--- TeamProject/app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/cahillcharles/TeamProject/SavedWorkouts.java
+++ TeamProject/app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/cahillcharles/TeamProject/SavedWorkouts.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SavedWorkouts extends AppCompatActivity {
 
@@ -15,17 +16,26 @@ public class SavedWorkouts extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_workouts);
 
+
+        //declare button for displaying saved workouts
         Button button = (Button) findViewById(R.id.workoutButton);
 
+        //declare button for going home
         Button workoutToHomeBtn = (Button) findViewById(R.id.workoutToHomeButton);
     }
 
+    //Function that sets the textview below the button to this text. Will be replace by fragments and generalized when we get to that step
     public void displayWorkout(View view) {
         TextView tView = findViewById(R.id.workoutTView);
         tView.setText("I dunno some workout\nLift big weight\nDo something");
+        Toast.makeText(this, "Displays whatever workout was saved", Toast.LENGTH_SHORT).show();
     }
+
+    //Button to navigate to home screen
+    //will be replaced by navigation bar when we get to that step
     public void workoutToHome(View view) {
         Intent intent = new Intent(SavedWorkouts.this, MainActivity.class);
+        Toast.makeText(this, "There's no place like home", Toast.LENGTH_SHORT).show();
 
         startActivity(intent);
     }
