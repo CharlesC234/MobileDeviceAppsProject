64ef6781738b44ef4cd933198e72bf4ba1ab2a5a
Zachary <zacharychair@gmail.com>
2023-10-16 22:14:48 -0400

    Updated layout for Saved Workouts screen + new button to go back to home screen + for realsies this time bc I think I did it wrong last time.

--- TeamProject/app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/cahillcharles/TeamProject/SavedWorkouts.java
+++ TeamProject/app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/cahillcharles/TeamProject/SavedWorkouts.java
@@ -2,6 +2,7 @@ package edu.floridapoly.mobiledeviceapps.fall23.cahillcharles.TeamProject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,10 +16,17 @@ public class SavedWorkouts extends AppCompatActivity {
         setContentView(R.layout.activity_saved_workouts);
 
         Button button = (Button) findViewById(R.id.workoutButton);
+
+        Button workoutToHomeBtn = (Button) findViewById(R.id.workoutToHomeButton);
     }
 
     public void displayWorkout(View view) {
         TextView tView = findViewById(R.id.workoutTView);
         tView.setText("I dunno some workout\nLift big weight\nDo something");
+    }
+    public void workoutToHome(View view) {
+        Intent intent = new Intent(SavedWorkouts.this, MainActivity.class);
+
+        startActivity(intent);
     }
 }
