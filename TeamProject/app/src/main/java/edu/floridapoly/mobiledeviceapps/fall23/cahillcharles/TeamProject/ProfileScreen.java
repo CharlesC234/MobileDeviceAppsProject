4b2edd02889b43acf79d7477c5c7393a911a41b9
Rodolphe Eugene <reugene2284@floridapoly.edu>
2023-10-25 15:49:05 -0400

    Finish the database and connecting profile screen to user info from sign in.

--- TeamProject/app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/cahillcharles/TeamProject/ProfileScreen.java
+++ TeamProject/app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/cahillcharles/TeamProject/ProfileScreen.java
@@ -2,19 +2,54 @@ package edu.floridapoly.mobiledeviceapps.fall23.cahillcharles.TeamProject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ProfileScreen extends AppCompatActivity {
+
+    // declare variables
+    private TextView nameTextView, emailTextView;
+    private DatabaseHelper databaseHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_screen);
         ImageView imgView = findViewById(R.id.profileImg);
         imgView.setImageResource(R.drawable.avatar);
+
+        // Initialize your databaseHelper
+        databaseHelper = new DatabaseHelper(this);
+
+        // referencing the name, email TextView
+        nameTextView = findViewById(R.id.nameTextView);
+        emailTextView = findViewById(R.id.emailTexView);
+
+
+        // get user data from database
+        Cursor cursor = databaseHelper.getUser();
+
+        // Grabbing the first name on the database
+        if(cursor.moveToLast()){
+            @SuppressLint("Range") String name = cursor.getString(cursor.getColumnIndex("name"));
+            @SuppressLint("Range") String email = cursor.getString(cursor.getColumnIndex("email"));
+
+
+            // Update the name Textview
+            nameTextView.setText(name);
+
+            // Update the email Textview
+            emailTextView.setText(email);
+        }
+        // closing the cursor
+        cursor.close();
+
     }
     public void toSavedWorkouts(View myView)
     {
