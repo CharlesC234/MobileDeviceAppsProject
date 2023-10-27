4d046ed0287065606018443f021c0ef6a985d27c
GabeALopez <109094200+GabeALopez@users.noreply.github.com>
2023-10-27 15:17:37 -0400

    Created code for workouts database

---
+++ TeamProject/app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/cahillcharles/TeamProject/WorkoutModelClass.java
@@ -0,0 +1,32 @@
+package edu.floridapoly.mobiledeviceapps.fall23.cahillcharles.TeamProject;
+
+public class WorkoutModelClass {
+
+    // declare variables
+    private String name, description;
+
+    // constructor
+    public WorkoutModelClass(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String email) {
+        this.description = description;
+    }
+
+
+}
